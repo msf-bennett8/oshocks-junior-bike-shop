04670df0ffff82dcb90c90ae7a35ee152310568d
msf-bennett8 <moituizablon8@gmail.com>
2025-10-15 16:36:34 +0300

    minimal fixes phase 6

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -23,6 +23,7 @@ return [
         'http://localhost:3000',
         'http://localhost:3001',
         'http://127.0.0.1:3000',
+        'https://oshocks-junior-bike-shop-5p59pxaxn.vercel.app',
         'https://oshocks-junior-bike-shop.vercel.app',
     ],
 
