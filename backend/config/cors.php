2e4a7a4eefb718faa3362b24a964cb501e726e8c
msf-bennett8 <moituizablon8@gmail.com>
2025-10-15 23:00:26 +0300

    Fix CORS for Vercel frontend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,46 +1,22 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    
     'allowed_methods' => ['*'],
-
+    
     'allowed_origins' => [
         'http://localhost:3000',
-        'http://localhost:3001',
-        'http://127.0.0.1:3000',
-        'https://oshocks-junior-bike-shop-96s0lix0n.vercel.app/dashboard',
-        'https://oshocks-junior-bike-shop-96s0lix0n.vercel.app',
-        'https://oshocks-junior-bike-shop-3a1jdoket.vercel.app',
-        'https://oshocks-junior-bike-shop-5p59pxaxn.vercel.app',
         'https://oshocks-junior-bike-shop.vercel.app',
-        'https://*.vercel.app',
     ],
-
-    'allowed_origins_patterns' => [
-        '/^https:\/\/.*\.vercel\.app$/',
-    ],
-
+    
+    'allowed_origins_patterns' => [],
+    
     'allowed_headers' => ['*'],
-
+    
     'exposed_headers' => [],
-
+    
     'max_age' => 0,
-
+    
     'supports_credentials' => true,
-
 ];
