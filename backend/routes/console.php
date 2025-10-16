65812ca31688e64b8ca7ed415258c068e361a39f
msf-bennett8 <moituizablon8@gmail.com>
2025-10-17 00:56:54 +0300

    Revert console.php auto-migrate - use manual approach

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -5,9 +5,4 @@ use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
-// Auto-run migrations on startup in production
-if (app()->environment('production')) {
-    Artisan::call('migrate', ['--force' => true]);
-}
+})->purpose('Display an inspiring quote')->isHidden();
