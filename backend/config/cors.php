ce86000f26919653daa6416e75225032060173ce
msf-bennett8 <moituizablon8@gmail.com>
2025-10-22 10:30:13 +0300

    Fix CORS allowed headers to be explicit

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -14,7 +14,7 @@ return [
     
     'allowed_origins_patterns' => [],
     
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
     
     'exposed_headers' => ['Authorization'],
     
